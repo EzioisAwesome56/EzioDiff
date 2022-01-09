2a9355e5b2a8e36c850601cab02b57587cda1f05
EzioisAwesome56 <jedijon567@gmail.com>
2022-01-09 13:29:39 -0500

    initial commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+# EzioDiff
+I couldnt think of a better name for this lol
+## What does it do?
+Compares 2 folders of files, and copies different files from the dir you are comparing to an output directory
+## Usage
+```Eziodiff.jar <source> <comp> <out>```
---
+++ src/main/java/com/eziosoft/EzioDiff/Main.java
@@ -0,0 +1,51 @@
+package com.eziosoft.EzioDiff;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Eziosoft file sorter");
+        if (args.length < 3){
+            System.err.println("Requires 3 arguments!");
+            System.err.println("1- path to original folder of files");
+            System.err.println("2- path to folder to compare");
+            System.err.println("3- path to output folder");
+            System.exit(-1);
+        }
+        File in = new File(args[0]);
+        File comp = new File(args[1]);
+        File out = new File(args[2]);
+        if (!in.exists() || !in.isDirectory()){
+            System.err.println("Error: input folder either doesnt exist or is file!");
+            System.err.println(in.getAbsolutePath());
+            System.exit(-1);
+        }
+        if (!comp.exists() || !comp.isDirectory()){
+            System.err.println("Error: comparison folder either doesnt exist or is file!");
+            System.err.println(comp.getAbsolutePath());
+            System.exit(-1);
+        }
+        if (!out.exists() || !out.isDirectory()){
+            System.err.println("Error: output folder either doesnt exist or is file!");
+            System.err.println(out.getAbsolutePath());
+            System.exit(-1);
+        }
+        File[] list = comp.listFiles();
+        int diff = 0;
+        for (File f : list){
+            if (!FileUtils.contentEquals(f, new File(in, f.getName()))){
+                Files.copy(Paths.get(f.getAbsolutePath()), Paths.get(new File(out, f.getName()).getAbsolutePath()), StandardCopyOption.REPLACE_EXISTING);
+                System.out.println(f.getName() + " is different from source directory");
+                diff++;
+            }
+        }
+        System.out.println("processed " + list.length + " files, " + diff + " different files copied.");
+    }
+}
